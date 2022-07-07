8e25131ce0a80dad327013f2f81c957d67773230
RoxxorXx <joseph.cabanis@gmail.com>
2022-07-07 23:41:02 +0200

    tests on win32 window

--- OwlGraph/Window.cpp
+++ OwlGraph/Window.cpp
@@ -1,6 +1,68 @@
 ﻿#include "Window.h"
 
-Owl::Graph::Window::Window()
+
+LRESULT CALLBACK Owl::Graph::Window::WindowProcedure(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
-	;
+	switch (Msg)
+	{
+	// case WM_DESTROY:
+	// 	PostQuitMessage(WM_QUIT);
+	// 	break;
+	case WM_PAINT:
+		{
+			// /* */
+			// PAINTSTRUCT ps;
+			// HDC hdc;
+			// RECT rc;
+			// hdc = BeginPaint(hwnd, &ps);
+			//
+			// GetClientRect(hwnd, &rc);
+			// SetTextColor(hdc, 0);
+			// SetBkMode(hdc, TRANSPARENT);
+			// DrawText(hdc, L"HELLO WORLD", -1, &rc, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
+			//
+			// EndPaint(hwnd, &ps);
+			// /* */
+			// break;
+		}
+		break;
+	default:
+		return DefWindowProc(hwnd, Msg, wParam, lParam);
+	}
+	return 0;
+}
+
+Owl::Graph::Window::Window(bool _isShow)
+{
+	LRESULT CALLBACK WndProc(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+	
+	WNDCLASSEX WndClsEx = {};
+	WndClsEx.cbSize = sizeof(WNDCLASSEX);
+	WndClsEx.lpfnWndProc = WndProc;
+	WndClsEx.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClsEx.lpszClassName = L"ClassName";
+	RegisterClassEx(&WndClsEx);
+
+	
+	m_windowHandle = CreateWindowEx(WS_EX_LEFT,
+		L"ClassName",
+		L"Window Title",
+		// (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX),
+		(WS_THICKFRAME | WS_MAXIMIZEBOX),
+		0,
+		0,
+		0,
+		0,
+		NULL,
+		NULL,
+		NULL,
+		NULL);
+	
+	SetVisible(_isShow);
+}
+
+void Owl::Graph::Window::SetVisible(bool _visible)
+{
+	ShowWindow(m_windowHandle, _visible ? SW_SHOW : SW_HIDE);
+	UpdateWindow(m_windowHandle);
 }
--- OwlGraph/Window.h
+++ OwlGraph/Window.h
@@ -1,16 +1,36 @@
 ﻿#pragma once
+#include <windows.h>
 
 namespace Owl::Graph
 {
 	class Window
 	{
+	private:
+		HWND m_windowHandle;
+
+		LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam);
 	public:
-		Window();
+		Window(bool _isShow = true);
 		~Window() = default;
 
 		Window(const Window& _other) = default;
 		Window(Window&& _other) noexcept = default;
 		Window& operator=(const Window& _other) = default;
 		Window& operator=(Window&& _other) noexcept = default;
+
+		void SetVisible(bool _visible = true);
+		bool IsVisible() const { return IsWindowVisible(m_windowHandle); } 
+
+		//test
+		void tupdate()
+		{
+			MSG Msg;
+			while (GetMessage(&Msg, m_windowHandle, 0, 0))
+			{
+				TranslateMessage(&Msg);
+				DispatchMessage(&Msg);
+			}
+
+		}
 	};
 }
